cbf413eda00b459bc51e8c6523eeee05b75279bb
mauricetjmurphy <mauricetjmurphy@gmail.com>
2023-03-01 20:50:43 +0700

    Add Makefile

--- frontend/src/components/Contact.js
+++ frontend/src/components/Contact.js
@@ -50,8 +50,7 @@ function Contact() {
       <h2 className="section-title">03. Whats Next?</h2>
       <h1 className="main-title">Get In Touch</h1>
       <p className="contact-body">
-        Although I'm not currently looking for any new opportunities, my inbox
-        is always open. Whether you have a question or just want to say hi, I'll
+        My inbox is always open. Whether you have a question or just want to say hi, I'll
         try my best to get back to you!
       </p>
       <a href="mailto:mauricetjmurphy@gmail.com" className="email-me">
